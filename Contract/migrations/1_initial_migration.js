f5d91ea44c12036f5138c4410cc1e811911ad25a
이인수 <in63119@gmail.com>
2023-01-06 23:12:37 +0900

    deploy setting

--- Contract/migrations/1_initial_migration.js
+++ Contract/migrations/1_initial_migration.js
@@ -3,6 +3,8 @@ const inNFT = artifacts.require("InNFT");
 
 // config
 const { address } = require("../env-config");
+const jsonUrl =
+  "https://gateway.pinata.cloud/ipfs/QmRGeNseaeeEoRBGWwRotapBPyKfZ9CrrHTvTEToNM8vn5";
 
 // const { makeInProxy, makeInNFT } = require("../data/makeJson");
 
@@ -11,8 +13,6 @@ const { address } = require("../env-config");
 module.exports = async function (deployer) {
   await deployer.deploy(inNFT);
   const inNFTContract = await inNFT.deployed();
-  await inNFTContract.mintNFT(address, imgUrl);
-
-  makeInNFT(inNFTContract.address);
+  await inNFTContract.mintNFT(address, jsonUrl);
   //   makeInProxy(inProxyContract.address);
 };
