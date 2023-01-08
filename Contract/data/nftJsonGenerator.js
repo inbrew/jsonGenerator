125b5f808763d264180afd70966d8bb38c0fa2ed
이인수 <in63119@gmail.com>
2023-01-08 22:20:17 +0900

    init setting NFT Json

--- Contract/data/nftJsonGenerator.js
+++ Contract/data/nftJsonGenerator.js
@@ -3,15 +3,25 @@ const path = require("path");
 const basePath = __dirname;
 let base = path.join(basePath, "/nftJson");
 
-const nftName = "I Graduation";
-const description = "Video Tape - I Graduation";
-const hiddenImgUrl =
-  "https://in-nft.s3.ap-northeast-2.amazonaws.com/%E1%84%92%E1%85%B4%E1%84%89%E1%85%AE_%E1%84%8B%E1%85%B5%E1%86%AB%E1%84%89%E1%85%AE(%E1%84%90%E1%85%A6%E1%84%8B%E1%85%B5%E1%84%91%E1%85%B3).mp3";
+const nftName = "H and I";
+const description = "Video Tape - H and I";
+const hiddenImgUrl = "https://youtu.be/fsGx3_Ot3zw";
+const ImgUrl = "https://in-nft.s3.ap-northeast-2.amazonaws.com/In_kid.png";
 const totalNum = 1;
 
+// 여러개
 try {
   for (let i = 1; i <= totalNum; i++) {
-    let json = `{"name":"${nftName} #${i}","description":"${description}","image":"${hiddenImgUrl}","attributes":[{"trait_type": "Unknown","value": "Unknown"}]}`;
+    let json = `{
+        "name": "${nftName} #${i}",
+        "description": "${description}",
+        "image": "${ImgUrl}",
+        "youtube_url": "${hiddenImgUrl}",
+        "attributes": {
+            "type": "object",
+            "description": "a IN NFT json"
+        }
+    }`;
 
     fs.writeFile(`${base}/${i}.json`, json, "utf8", (e) => e);
   }
