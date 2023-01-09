35e64b3db2a01ec3b46c3b995586b6c64d93881c
inbrew <insu@a-fun.co.kr>
2023-01-09 10:56:28 +0900

    init setting Client

---
+++ Client/src/components/Loading.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+// MUI css
+import { Box, LinearProgress } from "@mui/material";
+
+export default function Main() {
+  return (
+    <Box>
+      <LinearProgress color="inherit" />
+    </Box>
+  );
+}
