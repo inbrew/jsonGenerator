18252887a4e1ee9a322fd41dc466dbbdb7bd8cea
inbrew <insu@a-fun.co.kr>
2023-01-10 16:08:13 +0900

    solve .env in client

--- Client/src/components/JsonButton.js
+++ Client/src/components/JsonButton.js
@@ -3,10 +3,12 @@ import React from "react";
 // MUI css
 import { Button } from "@mui/material";
 
+// api
+import { getApiCall } from "../APIs/apicallAWS";
 
 export default function JsonButton() {
-    const handleCreateButton = () => {
-        console.log("잘눌림");
+    const handleCreateButton = async () => {
+        await getApiCall();
     }
 
     return (
--- Client/src/config.js
+++ Client/src/config.js
@@ -1,3 +1,5 @@
 module.exports = {
     localServerUrl: "http://localhost:4000",
+    accessKeyId: process.env.REACT_APP_ACCESS_KEY_ID,
+    secretAccessKey: process.env.REACT_APP_SECRET_ACCESS_KEY
 }
