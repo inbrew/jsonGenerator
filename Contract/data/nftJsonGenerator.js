8d2f4a12d137d94451ad4f285a17838bca45e086
이인수 <in63119@gmail.com>
2023-01-12 22:40:37 +0900

    test

--- Contract/data/nftJsonGenerator.js
+++ Contract/data/nftJsonGenerator.js
@@ -1,5 +1,5 @@
 const path = require("path");
-require('dotenv').config({ path: path.resolve(__dirname, "../.env") });
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 const base = path.resolve(__dirname, "../nftData");
 const jsonBase = path.resolve(__dirname, "../nftJson");
 
@@ -7,19 +7,26 @@ const jsonBase = path.resolve(__dirname, "../nftJson");
 const accessKey = process.env.ACCESS_KEY;
 const secretAccessKey = process.env.SECRET_ACCESS_KEY;
 const jwt = process.env.JWT_KEY;
-const pinataUrl = 'https://api.pinata.cloud/pinning/pinFileToIPFS';
+
+// pinata
+const pinataSDK = require("@pinata/sdk");
+const pinata = new pinataSDK({
+  pinataApiKey: accessKey,
+  pinataSecretApiKey: secretAccessKey,
+});
+const pinataUrl = "https://api.pinata.cloud/pinning/pinFileToIPFS";
+const pinataJsonUrl = "https://api.pinata.cloud/pinning/pinJSONToIPFS";
 
 const axios = require("axios");
 const fs = require("fs");
-const FormData = require('form-data');
+const FormData = require("form-data");
 
 const nftDataFiles = fs.readdirSync(base);
 const nftJsonFiles = fs.readdirSync(jsonBase);
 
 // const { pinFileToIPFS } = require("./makeTokenUri");
 
-const pinIMGToIPFS = async () => {
-
+const pinIMGToIPFS = async (reqData) => {
   let imgUrl = [];
 
   for (let i = 0; i < nftDataFiles.length; i++) {
@@ -30,16 +37,59 @@ const pinIMGToIPFS = async () => {
       maxContentLength: "Infinity",
       headers: {
         "Content-Type": `multipart/form-data; boundary=${data._boundary}`,
-        pinata_api_key: accessKey,
-        pinata_secret_api_key: secretAccessKey,
+        pinata_api_key: `${accessKey}`,
+        pinata_secret_api_key: `${secretAccessKey}`,
       },
     });
 
     imgUrl.push(`https://gateway.pinata.cloud/ipfs/${res.data.IpfsHash}`);
   }
 
-  return imgUrl;
-}
+  //   return imgUrl;
+
+  console.log("img : ", imgUrl);
+
+  // 여기서부터 복사
+  const metaDataJson = `{
+    "name": "${reqData.name}",
+    "description": "${reqData.description}",
+    "image": "${imgUrl[0]}"
+  }`;
+
+  fs.writeFile(
+    `${jsonBase}/${reqData.name}.json`,
+    metaDataJson,
+    "utf8",
+    (e) => e
+  );
+
+  //   let jsonData = new FormData();
+  //   jsonData.append(
+  //     "file",
+  //     fs.createReadStream(`${jsonBase}/${nftJsonFiles[0]}`)
+  //   );
+  //   let tokenUrl = [];
+
+  try {
+    // console.log("여긴 들어와??");
+    const stream = fs.createReadStream(`${jsonBase}/${nftJsonFiles[0]}`);
+    const result = await pinata
+      .pinJSONToIPFS(stream, {
+        pinataMetadata: { name: reqData.name },
+      })
+      .then((res) => {
+        console.log(res);
+        return res;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    return result;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 const pinFileToIPFS = async (data, imgUrl) => {
   const metaDataJson = `{
@@ -50,73 +100,89 @@ const pinFileToIPFS = async (data, imgUrl) => {
 
   fs.writeFile(`${jsonBase}/${data.name}.json`, metaDataJson, "utf8", (e) => e);
 
-  let tokenUrl = [];
+  //   let jsonData = new FormData();
+  //   jsonData.append(
+  //     "file",
+  //     fs.createReadStream(`${jsonBase}/${nftJsonFiles[0]}`)
+  //   );
 
-  if (nftJsonFiles.length > 0) {
-    for (let i = 0; i < nftJsonFiles.length; i++) {
-      let jsonData = new FormData();
-      jsonData.append("file", fs.createReadStream(`${jsonBase}/${nftJsonFiles[i]}`));
+  const stream = fs.createReadStream(`${jsonBase}/${nftJsonFiles[0]}`);
+  //   let tokenUrl = [];
+  const result = await pinata
+    .pinJSONToIPFS(stream, {
+      pinataMetadata: { name: data.name },
+      pinataOptions: {
+        cidVersion: 0,
+      },
+    })
+    .then((res) => {
+      console.log(res);
+      return res;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  //   if (nftJsonFiles.length > 0) {
+  //     for (let i = 0; i < nftJsonFiles.length; i++) {
+  // let jsonData = new FormData();
+  // jsonData.append(
+  //   "file",
+  //   fs.createReadStream(`${jsonBase}/${nftJsonFiles[i]}`)
+  // );
 
-      const pinataMetaData = JSON.stringify({
-        "pinataMetadata": {
-          "name": data.name
-        },
-        "pinataContent": {
-          "data": metaDataJson
-        }
-      })
+  //       try {
+  //         const jsonRes = await axios
+  //           .post(
+  //             pinataJsonUrl,
+  //             {
+  //               pinataMetadata: {
+  //                 name: "test",
+  //               },
 
-      try {
-        const jsonRes = await axios.post('https://api.pinata.cloud/pinning/pinJSONToIPFS', {
+  //               pinataContent: jsonData,
+  //             },
+  //             {
+  //               headers: {
+  //                 pinata_api_key: `${accessKey}`,
+  //                 pinata_secret_api_key: `${secretAccessKey}`,
+  //               },
+  //             }
+  //           )
+  //           .then((res) => {
+  //             console.log(res);
+  //           })
+  //           .catch((err) => {
+  //             console.log(err);
+  //           });
 
-          maxContentLength: 'Infinity',
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${jwt}`,
-          },
+  //         tokenUrl.push(
+  //           `https://gateway.pinata.cloud/ipfs/${jsonRes.data.IpfsHash}`
+  //         );
+  //       } catch (err) {
+  //         console.log(err);
+  //       }
+  //     }
+  //     console.log("tokenUrl : ", tokenUrl);
+  //     return tokenUrl;
+  //   }
 
-          data: pinataMetaData
-        }).then(res => {
-          console.log(res);
-        }).catch(err => {
-          console.log(err);
-        })
-
-        tokenUrl.push(`https://gateway.pinata.cloud/ipfs/${jsonRes.data.IpfsHash}`);
-
-      } catch (err) {
-        console.log(err);
-      }
-
-
-
-    }
-    console.log("tokenUrl : ", tokenUrl);
-    return tokenUrl;
-  }
-
-  return "들어오지 않았습니다."
-
-}
-
+  return result;
+};
 
 module.exports = {
   getNFTJson: async (data) => {
-    const imgUrl = await pinIMGToIPFS();
+    const imgUrl = await pinIMGToIPFS(data);
 
     if (imgUrl) {
-      console.log("이미지 유알엘은 들어옴");
-      const tokenUri = await pinFileToIPFS(data, imgUrl);
-      console.log(tokenUri);
-      return tokenUri;
+      //   console.log("이미지 유알엘은 들어옴", imgUrl);
+      //   const tokenUri = await pinFileToIPFS(data, imgUrl);
+      //   console.log(tokenUri);
+      //   return tokenUri;
+      console.log("과연? : ", imgUrl);
+      return imgUrl;
     }
-
-
-
-  }
-}
-
-
+  },
+};
 
 // const nftName = "H and I";
 // const description = "Video Tape - H and I";
